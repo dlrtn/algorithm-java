63751e9dff3d2ec4c3e4f2005bf07e1e07d459da
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-23 18:27:24 +0900

    6번과제 조건에 맞춰 수정

--- src/zeroBaseFirstAndSecondWeeks/JavaStudy06/Main.java
+++ src/zeroBaseFirstAndSecondWeeks/JavaStudy06/Main.java
@@ -1,3 +1,6 @@
+/*
+주익수
+*/
 package zeroBaseFirstAndSecondWeeks.JavaStudy06;
 
 import java.util.Random;
@@ -15,7 +18,7 @@ public class Main { //주익수
 
         int max = 0;
 
-        for (int i = 1; i <= 100000; i++) {
+        for (int i = 1; i <= 10000; i++) {
 
             int temp = rand.nextInt(4);
 
@@ -23,7 +26,7 @@ public class Main { //주익수
             if (max < presidentialCandidateCount[temp])
                 max = presidentialCandidateCount[temp];
 
-            System.out.println("[투표진행율]: " + String.format("%.2f", ((double) i / 100000) * 100) + "%, " + i + "명 투표 => " + presidentialCandidate[temp]);
+            System.out.println("[투표진행율]: " + String.format("%.2f", ((double) i / 10000) * 100) + "%, " + i + "명 투표 => " + presidentialCandidate[temp]);
             System.out.println("[기호:1] 이재명: " + String.format("%.2f", ((double) presidentialCandidateCount[0] / i * 100)) + "%" + ", (투표수: " + presidentialCandidateCount[0] + ")");
             System.out.println("[기호:2] 윤석열: " + String.format("%.2f", ((double) presidentialCandidateCount[1] / i * 100)) + "%" + ", (투표수: " + presidentialCandidateCount[1] + ")");
             System.out.println("[기호:3] 심상정: " + String.format("%.2f", ((double) presidentialCandidateCount[2] / i * 100)) + "%" + ", (투표수: " + presidentialCandidateCount[2] + ")");
